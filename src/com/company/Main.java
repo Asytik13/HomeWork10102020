9381b30846fafb5963248dff346d0d46b46ce143
olga <pakhomovaolga96@gmail.com>
2020-10-15 21:26:10 +0200

    Task1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,24 @@
 package com.company;
-/*
 public class Main {
+
     public static void main(String[] args) {
-        void person = new Person();
+        void person = new Person(); // need to be Person person = new Person();
         person.setNameAndAge("John", 35);
-        int greet = person.greet;
+        int greet = person.greet; // need to be String greet = person.greet()
         System.out.println(greet);
     }
 }
 
-
-
 class Person {
     String name;
-    int age
+    int age // missed ‘;’
+
     void setNameAndAge(String newName, int newAge) {
         name = newName;
-        age = newAge
+        age = newAge // missed ‘;’
     }
+
     String greet() {
         return "I am " + name + ", " + age + " years old";
     }
 }
-*/
